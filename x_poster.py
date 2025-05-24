b9c089564a9913ca8afc2e4792ffa403b45f82d7
coder-zim <c0d3r.zim@gmail.com>
2025-05-24 15:34:13 -0400

    More integration FC >>to>> FD

--- x_poster.py
+++ x_poster.py
@@ -30,12 +30,12 @@ class XPoster:
             logger.info("X integration disabled")
 
     def post_report(self, address, chain, report):
-        if not self.api or not CONFIG["FARTCAT_X_LAUNCH"]:
+        if not self.api or not CONFIG["FARTDOG_X_LAUNCH"]:
             logger.info("X posting skipped (disabled or not configured)")
             return
         try:
             clean_text = re.sub(r'<[^>]+>', '', report)
-            summary = f"{CONFIG['BOT_NAME']} sniffed {address} on {chain.title()}! 💩🐶\n{clean_text[:200]}...\nSniff it: https://t.me/FartdogBot"
+            summary = f"{CONFIG['BOT_NAME']} sniffed {address} on {chain.title()}! 💩🐶\n{clean_text[:200]}...\nSniff it: https://t.me/fartdog_bot"
             self.api.update_status(summary)
             logger.info(f"Posted to X for {address} on {chain}")
         except Exception as e:

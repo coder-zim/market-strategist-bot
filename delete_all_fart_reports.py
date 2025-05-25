8c00fb012155ea83f4553098ee7030d3b8e0e697
coder-zim <c0d3r.zim@gmail.com>
2025-05-24 20:28:26 -0400

    Key removed and UI improved

--- delete_all_fart_reports.py
+++
@@ -1,4 +0,0 @@
-import requests
-token = "7901149869:AAGcXpRyrh0r1WWTxOEDEjWY2nK6IJhX5qQ"
-res = requests.get(f"https://api.telegram.org/bot{token}/deleteWebhook?drop_pending_updates=true")
-print(res.json())

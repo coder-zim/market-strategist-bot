7c6266f3744411ecbe4e42716ef2d38c82992d49
coder-zim <c0d3r.zim@gmail.com>
2025-05-26 16:04:14 -0400

    Changed asyncio to run_until_complete

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,20 +1,21 @@
 # telegram_bot.py
-from telegram.ext import ApplicationBuilder
-from fartdog_handler import get_conversation_handler
-from dotenv import load_dotenv
 import os
 import asyncio
+from dotenv import load_dotenv
+from telegram.ext import ApplicationBuilder
+from fartdog_handler import get_conversation_handler
+import nest_asyncio
 
 load_dotenv()
+nest_asyncio.apply()
+
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 async def main():
     print("✅ Starting Fartdog bot...")
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-
     app.add_handler(get_conversation_handler())
-
     await app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.get_event_loop().run_until_complete(main())
